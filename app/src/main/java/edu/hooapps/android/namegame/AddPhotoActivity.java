d98cccb039e96ee26bf2f1bf8863793fabc3df47
spencer.gennari <spencer.gennari@willowtreeapps.com>
2015-09-19 02:03:17 -0400

    Added activities and fragments for the game screen and add photo screen

---
+++ app/src/main/java/edu/hooapps/android/namegame/AddPhotoActivity.java
@@ -0,0 +1,15 @@
+package edu.hooapps.android.namegame;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class AddPhotoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_photo);
+    }
+}
--- app/src/main/java/edu/hooapps/android/namegame/MainActivityFragment.java
+++ app/src/main/java/edu/hooapps/android/namegame/MainActivityFragment.java
@@ -1,15 +1,20 @@
 package edu.hooapps.android.namegame;
 
+import android.content.Intent;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class MainActivityFragment extends Fragment {
+
+    private Button playButton;
+    private Button addPhotoButton;
 
     public MainActivityFragment() {
     }
@@ -17,6 +22,34 @@ public class MainActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+        playButton = (Button) rootView.findViewById(R.id.button_play);
+        addPhotoButton = (Button) rootView.findViewById(R.id.button_camera);
+
+        playButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startGameActivity();
+            }
+        });
+
+        addPhotoButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startAddPhotoActivity();
+            }
+        });
+
+        return rootView;
+    }
+
+    private void startGameActivity() {
+        Intent gameIntent = new Intent(getActivity(), GameActivity.class);
+        startActivity(gameIntent);
+    }
+
+    private void startAddPhotoActivity() {
+        Intent addPhotoIntent = new Intent(getActivity(), AddPhotoActivity.class);
+        startActivity(addPhotoIntent);
     }
 }
